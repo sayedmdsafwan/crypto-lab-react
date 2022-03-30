7ba484385b6dd9823592561c21f5d625e8755c77
Md. Safwan <sayedmdsafwan@gmail.com>
2022-03-30 20:35:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Homepage from "./components/Homepage/Homepage";
+import Coins from "./components/Coins/Coins";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Crypto Cafe Starter Template</h1>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/coins" element={<Coins />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    );
+};
 
-export default App
+export default App;
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
-  return (
-    <div className='md:px-16 py-8'>
-      <div className='flex flex-col justify-center h-[80vh] items-center'>
-        <h1 className=' font-mono text-xl md:text-5xl '>
-          Welcome to CRYPTO CAFE<span className='text-6xl'>☕</span>
-        </h1>
-        <button className='py-2 px-3 text-white mt-3 rounded-full bg-cyan-600'>
-          Explore Coins
-        </button>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="md:px-16 py-8">
+            <div className="flex flex-col justify-center h-[80vh] items-center">
+                <h1 className=" font-mono text-xl md:text-5xl ">
+                    Welcome to CRYPTO LAB !<span className="text-6xl">☕</span>
+                </h1>
+                <Link to="/coins">
+                    <button className="py-2 px-4 text-white mt-3 rounded bg-violet-600">
+                        Explore Coins
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
+};
 
-export default Homepage
+export default Homepage;
