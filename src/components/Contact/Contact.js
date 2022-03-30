999521145bd6d5b9075bb1eee3085ddd72eb025d
Md. Safwan <sayedmdsafwan@gmail.com>
2022-03-30 21:00:07 +0600

    design changed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 
 const Contact = () => {
     return (
-        <section className="px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2">
+        <section className="px-4 pt-20 pb-24 mx-auto w-4/5 md:px-2">
             <div className="flex flex-col justify-center items-center">
                 <h1 className=" font-mono text-xl text-center md:text-3xl ">
                     Contact Us, Our Office Address Down Below
